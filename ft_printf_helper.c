e5e86b133e598834a712d93678f5360640ff2e1a
Henrike Wiemann <hwiemann@c4b9c2.42berlin.de>
2023-06-07 11:50:44 +0200

    start wednesday

--- ft_printf_helper.c
+++ ft_printf_helper.c
@@ -6,19 +6,61 @@
 /*   By: hwiemann <hwiemann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:33:27 by hwiemann          #+#    #+#             */
-/*   Updated: 2023/06/05 15:37:47 by hwiemann         ###   ########.fr       */
+/*   Updated: 2023/06/06 21:14:04 by hwiemann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-size_t    ft_wrt_char(va_arg(ap, int))
+size_t    ft_wrt_char(int c)
+{
+    write(1, &c, 1);
+    return(1);
+}
 
-size_t ft_wrt_str(va_arg(ap, char *))
+size_t ft_wrt_str(char *s)
+{
+    size_t  i;
 
-size_t ft_wrt_hexpoint(va_arg(ap, void *))
+    if (s == NULL)
+    return (0);
+    i = 0;
+    while (*s)
+    {
+        write(1, s++, 1);
+        i++;
+    }
+    return (i);
+}
 
-size_t ft_wrt_nbr(va_arg(ap, int))
+size_t ft_wrt_base_ptr(va_arg(ap, int))
+{
+    unsigned long long  lnbr;
+    unsigned int    base_len;
+    size_t  i;
 
-size_t ft_wrt_unsnbr(va_arg(ap, unsigned int))
+    
+}
+
+size_t ft_wrt_base_nbr(int nbr, char *base)
+{
+    int base_len;
+    long    lnbr;
+    size_t  i;
+
+    base_len = ft_strlen(base);
+    lnbr = nbr;
+    i = 0;
+    if (lnbr < 0)
+    {
+        i += ft_wrt_char('-');
+        lnbr *= -1;
+    }
+    if (lnbr >= base_len)
+        i += ft_wrt_base_nbr(lnbr / base_len, base);
+        i += ft_wrt_char(base[lnbr % base_len]);
+    return (i);
+}
+
+size_t ft_wrt_base_unsnbr(va_arg(ap, unsigned int))
 
---
+++ test_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int	main()
+{
+	printf("  ");
+	printf(23);
+	printf();
+	printf(halo);
+	printf(g);
+	printf('g');
+	return (0);
+}
+
